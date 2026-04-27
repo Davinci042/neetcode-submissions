37e797a396a5aab8a784fcd6ab789f5e6d007c4b
Davinci042 <103445073+Davinci042@users.noreply.github.com>
2026-04-26 23:04:04 -0600

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,37 @@
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        S = {}
+        T = {}
+        
+        if len(s) != len(t):
+            return False
+        i = 0
+        while i < (len(alphabets)):
+            s_count = 0
+            t_count = 0
+
+            for char in s:
+                if alphabets[i] == char:
+                    s_count += 1
+
+            for char in t:
+                if alphabets[i] == char:
+                    t_count += 1
+
+            if s_count > 0:
+                S[alphabets[i]] = s_count
+
+            if t_count > 0:
+                T[alphabets[i]] = t_count
+
+            i += 1
+        
+        if S == T:
+            return True
+        else:
+            return False
+
+
+
